b67e413a2feebce3edb2766debfdc8c716d7fef4
ksmin23 <ubuntu@ip-172-31-8-230.ec2.internal>
2020-07-12 13:15:20 +0000

    add labmda function to merge small files

--- data_analytics_system/data_analytics_system_stack.py
+++ data_analytics_system/data_analytics_system_stack.py
@@ -14,7 +14,9 @@ from aws_cdk import (
   aws_kinesis as kinesis,
   aws_kinesisfirehose,
   aws_logs,
-  aws_elasticsearch
+  aws_elasticsearch,
+  aws_events,
+  aws_events_targets
 )
 
 from aws_cdk.aws_lambda_event_sources import (
@@ -263,4 +265,75 @@ class DataAnalyticsSystemStack(core.Stack):
       log_group_name="/aws/lambda/UpsertToES",
       retention=aws_logs.RetentionDays.THREE_DAYS)
     log_group.grant_write(upsert_to_es_lambda_fn)
+
+    merge_small_files_lambda_fn = _lambda.Function(self, "MergeSmallFiles",
+      runtime=_lambda.Runtime.PYTHON_3_7,
+      function_name="MergeSmallFiles",
+      handler="athena_ctas.lambda_handler",
+      description="Merge small files in S3",
+      code=_lambda.Code.asset("./src/main/python/MergeSmallFiles"),
+      environment={
+        #TODO: MUST set appropriate environment variables for your workloads.
+        'OLD_DATABASE': 'mydatabase',
+        'OLD_TABLE_NAME': 'retail_trans_json',
+        'NEW_DATABASE': 'mydatabase',
+        'NEW_TABLE_NAME': 'ctas_retail_trans_parquet',
+        'WORK_GROUP': 'primary',
+        'OUTPUT_PREFIX': os.path.join(s3_bucket.bucket_name, 'parquet-retail-trans'),
+        'STAGING_OUTPUT_PREFIX': os.path.join(s3_bucket.bucket_name, 'tmp'),
+        'COLUMN_NAMES': 'invoice,stockcode,description,quantity,invoicedate,price,customer_id,country',
+      },
+      timeout=core.Duration.minutes(5)
+    )
+
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["athena:*"]))
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["s3:Get*",
+        "s3:List*",
+        "s3:AbortMultipartUpload",
+        "s3:PutObject",
+      ]))
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["glue:CreateDatabase",
+        "glue:DeleteDatabase",
+        "glue:GetDatabase",
+        "glue:GetDatabases",
+        "glue:UpdateDatabase",
+        "glue:CreateTable",
+        "glue:DeleteTable",
+        "glue:BatchDeleteTable",
+        "glue:UpdateTable",
+        "glue:GetTable",
+        "glue:GetTables",
+        "glue:BatchCreatePartition",
+        "glue:CreatePartition",
+        "glue:DeletePartition",
+        "glue:BatchDeletePartition",
+        "glue:UpdatePartition",
+        "glue:GetPartition",
+        "glue:GetPartitions",
+        "glue:BatchGetPartition"
+      ]))
+    merge_small_files_lambda_fn.add_to_role_policy(aws_iam.PolicyStatement(
+      effect=aws_iam.Effect.ALLOW,
+      resources=["*"],
+      actions=["lakeformation:GetDataAccess"]))
+
+    lambda_fn_target = aws_events_targets.LambdaFunction(merge_small_files_lambda_fn)
+    aws_events.Rule(self, "ScheduleRule",
+      schedule=aws_events.Schedule.cron(minute="5"),
+      targets=[lambda_fn_target]
+    )
+
+    log_group = aws_logs.LogGroup(self, "MergeSmallFilesLogGroup",
+      log_group_name="/aws/lambda/MergeSmallFiles",
+      retention=aws_logs.RetentionDays.THREE_DAYS)
+    log_group.grant_write(merge_small_files_lambda_fn)
 
