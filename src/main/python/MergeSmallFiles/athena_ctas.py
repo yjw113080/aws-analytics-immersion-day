46a4061f72ef705b40544670d4bc6ce9c8a962d8
ksmin23 <sonagy23@gmail.com>
2020-11-22 22:57:54 +0900

    update test code for athena ctas query

--- src/main/python/MergeSmallFiles/athena_ctas.py
+++ src/main/python/MergeSmallFiles/athena_ctas.py
@@ -145,22 +145,27 @@ def lambda_handler(event, context):
   client = boto3.client('athena', region_name=AWS_REGION)
   run_drop_tmp_table(client, prev_basic_dt)
 
-  print('[INFO] Wait for a few seconds until dropping old table', file=sys.stderr)
-  time.sleep(10)
+  if not DRY_RUN:
+    print('[INFO] Wait for a few seconds until dropping old table', file=sys.stderr)
+    time.sleep(10)
 
   run_alter_table_add_partition(client, basic_dt,
     database_name=OLD_DATABASE,
     table_name=OLD_TABLE_NAME,
     output_prefix=OLD_TABLE_LOCATION_PREFIX)
-  print('[INFO] Wait for a few seconds until adding partitions to table: %s.%s' % (OLD_DATABASE, OLD_TABLE_NAME), file=sys.stderr)
-  time.sleep(10)
+
+  if not DRY_RUN:
+    print('[INFO] Wait for a few seconds until adding partitions to table: %s.%s' % (OLD_DATABASE, OLD_TABLE_NAME), file=sys.stderr)
+    time.sleep(10)
 
   run_alter_table_add_partition(client, basic_dt,
     database_name=NEW_DATABASE,
     table_name=NEW_TABLE_NAME,
     output_prefix=OUTPUT_PREFIX)
-  print('[INFO] Wait for a few seconds until adding partitions to table: %s.%s' % (NEW_DATABASE, NEW_TABLE_NAME), file=sys.stderr)
-  time.sleep(10)
+
+  if not DRY_RUN:
+    print('[INFO] Wait for a few seconds until adding partitions to table: %s.%s' % (NEW_DATABASE, NEW_TABLE_NAME), file=sys.stderr)
+    time.sleep(10)
 
   run_ctas(client, basic_dt)
 
@@ -171,8 +176,42 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-dt', '--basic-datetime', default=datetime.datetime.today().strftime('%Y-%m-%dT%H:05:00Z'),
     help='The scheduled event occurrence time ex) 2020-02-28T03:05:00Z')
+  parser.add_argument('--region-name', default='us-east-1',
+    help='aws region name')
+  parser.add_argument('--old-database', default='mydatabase',
+    help='aws athena source database name used by ctas query')
+  parser.add_argument('--old-table-name', default='retail_trans_json',
+    help='aws athena source table name used by ctas query')
+  parser.add_argument('--new-database', default='mydatabase',
+    help='aws athena target database name for merged files')
+  parser.add_argument('--new-table-name', default='ctas_retail_trans_parquet',
+    help='aws athena target table name for merged files')
+  parser.add_argument('--work-group', default='primary',
+    help='aws athena work group')
+  parser.add_argument('--old-table-location-prefix', required=True,
+    help='s3 path for aws athena source table')
+  parser.add_argument('--output-prefix', required=True,
+    help='s3 path for aws athena target table')
+  parser.add_argument('--staging-output-prefix', required=True,
+    help='s3 path for aws athena tmp table')
+  parser.add_argument('--column-names', default='*',
+    help='selectable column names of aws athena source table')
+  parser.add_argument('--run', action='store_true',
+    help='run ctas query')
 
   options = parser.parse_args()
+
+  DRY_RUN = False if options.run else True
+  REGION_NAME = options.region_name
+  OLD_DATABASE = options.old_database
+  OLD_TABLE_NAME= options.old_table_name
+  NEW_DATABASE = options.new_database
+  NEW_TABLE_NAME = options.new_table_name
+  WORK_GROUP = options.work_group
+  OLD_TABLE_LOCATION_PREFIX = options.old_table_location_prefix
+  OUTPUT_PREFIX = options.output_prefix
+  STAGING_OUTPUT_PREFIX = options.staging_output_prefix
+  COLUMN_NAMES = options.column_names
 
   event = {
     "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
